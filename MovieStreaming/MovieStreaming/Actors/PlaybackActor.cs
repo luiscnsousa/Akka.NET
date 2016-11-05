39819af05164108a202603dd3d35e6afe4c1a403
Luís Sousa <luiscnsousa@outlook.com>
2016-11-05 16:58:24 +0000

    added modules 1 and 2 source code

---
+++ MovieStreaming/MovieStreaming/Actors/PlaybackActor.cs
@@ -0,0 +1,37 @@
+﻿namespace MovieStreaming.Actors
+{
+    using System;
+    using System.Threading.Tasks;
+    using Akka.Actor;
+    using Messages;
+
+    public class PlaybackActor : ReceiveActor
+    {
+        public PlaybackActor()
+        {
+            Console.WriteLine("Creating a PlaybackActor");
+
+            this.Receive<PlayMovieMessage>(
+                message => HandlePlayMovieMessage(message), 
+                message => message.UserId == 42);
+        }
+
+        private void HandlePlayMovieMessage(PlayMovieMessage message)
+        {
+            Console.WriteLine("Received movie title " + message.MovieTitle);
+            Console.WriteLine("Received user Id " + message.UserId);
+        }
+
+        //protected override void OnReceive(object message)
+        //{
+        //    if (message is PlayMovieMessage)
+        //    {
+        //        var m = message as PlayMovieMessage;
+        //    }
+        //    else
+        //    {
+        //        Unhandled(message);
+        //    }
+        //}
+    }
+}
---
+++ MovieStreaming/MovieStreaming/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace MovieStreaming
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Akka.Actor;
+    using MovieStreaming.Actors;
+    using MovieStreaming.Messages;
+
+    class Program
+    {
+        private static ActorSystem MovieStreamingActorSystem;
+
+        static void Main(string[] args)
+        {
+            MainAsync().Wait();
+        }
+
+        static async Task MainAsync()
+        {
+            MovieStreamingActorSystem = ActorSystem.Create("MovieStreamingActorSystem");
+            Console.WriteLine("Actor system created");
+
+            Props playbackActorProps = Props.Create<PlaybackActor>();
+
+            IActorRef playbackActorRef = MovieStreamingActorSystem.ActorOf(playbackActorProps, "PlaybackActor");
+
+            playbackActorRef.Tell(new PlayMovieMessage("Akka.NET: The Movie", 42));
+            playbackActorRef.Tell(new PlayMovieMessage("Akka.NET: The Movie", 43));
+
+            Console.ReadLine();
+            await MovieStreamingActorSystem.Terminate();
+        }
+    }
+}
